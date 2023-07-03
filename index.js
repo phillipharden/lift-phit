2b21698ee539099fee442bf609f5851716725368
Phillip Harden <phillipharden@Phillips-MacBook-Pro.local>
2023-07-03 19:57:48 -0400

    updated index.js and config

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
+const port = process.env.PORT || 4444;
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const axios = require("axios");
-require('dotenv').config();
-
-const usersCtrl = require("./api/controllers/users");
-const workoutsCtrl = require("./api/controllers/workouts");
-const exercisesCtrl = require("./api/controllers/exercises");
-const setsCtrl = require("./api/controllers/sets");
-const authCtrl = require("./api/controllers/auth");
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const cors = require("cors");
 app.use(
   cors({
@@ -23,15 +15,25 @@ app.use(
     allowCrossDomain: true,
   })
 );
+const usersCtrl = require("./api/controllers/users");
+const workoutsCtrl = require("./api/controllers/workouts");
+const exercisesCtrl = require("./api/controllers/exercises");
+const setsCtrl = require("./api/controllers/sets");
+const authCtrl = require("./api/controllers/auth");
 
 const session = require("express-session");
 app.use(
   session({
-    saveUninitialized: false,
-    secret: "keyboardcat",
+    secret: "monkey_fist",
     cookie: { maxAge: 60000 },
+    saveUninitialized: false,
   })
 );
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+app.get("/api", (request, response, next) => {
+  response.send("Sunday Setlist API.");
+});
 
 app.get("/", (req, res) => {
   res.send("Home Page"); 
@@ -43,14 +45,23 @@ app.use("/exercises", exercisesCtrl);
 app.use("/sets", setsCtrl);
 app.use("/auth", authCtrl);
 
-÷
+const path = require("path");
+app.use(express.static(path.join(__dirname, "./reactjs/build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./reactjs/build", "index.html"));
 });
+
 
 // http://localhost:4444/ in the web browser
 
-^ app.listen(process.env.PORT || 4444, function () {
-^   console.log("Server is running on Heroku");
-^ }); // http://localhost:4444/ in the web browser
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+
+
+
+
+
+
 
 // COLORFUL COMMENTS
 //! Red (!)
